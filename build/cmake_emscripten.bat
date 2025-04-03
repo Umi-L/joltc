b2963e5dd41b347bf19ccf2c2576993b0b1a6225
Julian O'Grady <julianogrady07@gmail.com>
2025-04-02 22:22:40 -0400

    multithreading working

--- build/cmake_emscripten.bat
+++ build/cmake_emscripten.bat
@@ -1,5 +1,5 @@
 @echo off
 mkdir emscripten
 cd emscripten
-emcmake cmake ../../
+emcmake cmake ../../ -DCMAKE_BUILD_TYPE=Debug -DJPH_BUILD_SHARED=OFF -DUSE_WASM_SIMD=ON -DCROSS_PLATFORM_DETERMINISTIC=ON
 echo Open emscripten and run `cmake --build .` to build the project.
